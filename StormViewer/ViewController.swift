ad1472f3c14c1dbc5cefd2b6caa7466e3ab02d40
mwanjajoel <joelsilverworks@gmail.com>
2017-01-28 14:20:28 +0300

    Wrapped up the Table View Main Screen

--- StormViewer/ViewController.swift
+++ StormViewer/ViewController.swift
@@ -38,6 +38,20 @@ class ViewController: UITableViewController {
 
 	}
 
+	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+
+		return pictures.count
+	}
+
+	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+		let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+		cell.textLabel?.text = pictures[indexPath.row]
+
+		return cell
+
+	}
+
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
